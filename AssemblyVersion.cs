1b94bd2982822c000eb01c2bad7cfb66514b6653
Suremaker <wojciech.kotlarski@gmail.com>
2013-12-03 23:56:01 +0000

    + added HtmlResultFormatter

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
---
+++ LightBDD/Results/Formatters/HtmlResultFormatter.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Web.UI;
+
+namespace LightBDD.Results.Formatters
+{
+	/// <summary>
+	/// Formats feature results as HTML.
+	/// </summary>
+	public class HtmlResultFormatter : IResultFormatter
+	{
+		private readonly string _styles;
+
+		/// <summary>
+		/// Formatter constructor.
+		/// </summary>
+		public HtmlResultFormatter()
+		{
+			_styles = ReadStyles();
+		}
+
+		private string ReadStyles()
+		{
+			using (var stream = GetType().Assembly.GetManifestResourceStream("LightBDD.Results.Formatters.styles.css"))
+			using (var reader = new StreamReader(stream))
+				return reader.ReadToEnd();
+		}
+
+		/// <summary>
+		/// Formats feature results.
+		/// </summary>
+		/// <param name="features">Features to format.</param>
+		public string Format(params IFeatureResult[] features)
+		{
+			using (var memory = new MemoryStream())
+			using (var stream = new StreamWriter(memory))
+			using (var writer = new HtmlTextWriter(stream, ""))
+			{
+				writer.NewLine = "";
+				WriteHeader(writer);
+				WriteFeatures(writer, features);
+				WriteFooter(writer);
+				writer.Flush();
+				return Encoding.Default.GetString(memory.ToArray());
+			}
+		}
+
+		private void WriteFooter(HtmlTextWriter writer)
+		{
+			writer.RenderEndTag();
+			writer.RenderEndTag();
+		}
+
+		private void WriteHeader(HtmlTextWriter writer)
+		{
+			writer.WriteLine("<!DOCTYPE HTML>");
+			writer.RenderBeginTag(HtmlTextWriterTag.Html);
+			writer.RenderBeginTag(HtmlTextWriterTag.Head);
+
+			writer.AddAttribute("charset", "UTF-8");
+			writer.RenderBeginTag(HtmlTextWriterTag.Meta);
+			writer.RenderEndTag();
+
+			WriteTag(writer, HtmlTextWriterTag.Title, null, "Summary");
+
+			WriteTag(writer, HtmlTextWriterTag.Style, null, _styles);
+
+			writer.RenderEndTag();
+			writer.RenderBeginTag(HtmlTextWriterTag.Body);
+		}
+
+		private void WriteFeatures(HtmlTextWriter writer, IEnumerable<IFeatureResult> features)
+		{
+			foreach (var feature in features)
+				WriteFeature(writer, feature);
+		}
+
+		private void WriteFeature(HtmlTextWriter writer, IFeatureResult feature)
+		{
+			WriteTag(writer, HtmlTextWriterTag.Div, "feature", () =>
+			{
+				WriteTag(writer, HtmlTextWriterTag.Div, "header", () =>
+				{
+					WriteTag(writer, HtmlTextWriterTag.Div, "title", () =>
+					{
+						WriteTag(writer, HtmlTextWriterTag.Span, "label", feature.Label);
+						writer.Write(feature.Name);
+					});
+					WriteTag(writer, HtmlTextWriterTag.Div, "description", feature.Description);
+				});
+				WriteTag(writer, HtmlTextWriterTag.Div, "scenarios", () =>
+				{
+					foreach (var scenario in feature.Scenarios)
+						WriteScenario(writer, scenario);
+				});
+			});
+		}
+
+		private void WriteScenario(HtmlTextWriter writer, IScenarioResult scenario)
+		{
+			WriteTag(writer, HtmlTextWriterTag.Div, "scenario", () =>
+			{
+				WriteTag(writer, HtmlTextWriterTag.Div, "title", () =>
+				{
+					WriteStatus(writer, scenario.Status);
+					WriteTag(writer, HtmlTextWriterTag.Span, "label", scenario.Label);
+					writer.Write(scenario.Name);
+				});
+				foreach (var step in scenario.Steps)
+					WriteStep(writer, step);
+				WriteTag(writer, HtmlTextWriterTag.Div, "details", scenario.StatusDetails);
+			});
+		}
+
+		private void WriteStep(HtmlTextWriter writer, IStepResult step)
+		{
+			WriteTag(writer, HtmlTextWriterTag.Div, "step", () =>
+			{
+				WriteStatus(writer, step.Status);
+				WriteTag(writer, HtmlTextWriterTag.Span, "number", string.Format("{0}.", step.Number));
+				writer.Write(step.Name);
+			});
+		}
+
+		private static void WriteStatus(HtmlTextWriter writer, ResultStatus status)
+		{
+			WriteTag(writer, HtmlTextWriterTag.Div, "status " + status.ToString().ToLowerInvariant(), status.ToString());
+		}
+
+		private void WriteTag(HtmlTextWriter writer, HtmlTextWriterTag tag, string className, Action contentRenderer)
+		{
+			writer.AddAttribute(HtmlTextWriterAttribute.Class, className);
+			writer.RenderBeginTag(tag);
+			contentRenderer();
+			writer.RenderEndTag();
+		}
+
+		private static void WriteTag(HtmlTextWriter writer, HtmlTextWriterTag tag, string className, string content)
+		{
+			if (content == null)
+				return;
+			if (className != null)
+				writer.AddAttribute(HtmlTextWriterAttribute.Class, className);
+			writer.RenderBeginTag(tag);
+			writer.Write(content.Trim());
+			writer.RenderEndTag();
+		}
+	}
+}
