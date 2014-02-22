49738f236903920138a04f239a7d32b6c3ee3742
Suremaker <wojciech.kotlarski@gmail.com>
2014-02-22 16:55:41 +0000

    modified packageAll.cmd to create NuGet directory updated .gitignore

--- packageAll.cmd
+++ packageAll.cmd
@@ -1,4 +1,5 @@
-.nuget\NuGet.exe pack -sym LightBDD.MbUnit\LightBDD.MbUnit.csproj -OutputDirectory NuGet -Prop Configuration=Release
-.nuget\NuGet.exe pack -sym LightBDD.NUnit\LightBDD.NUnit.csproj -OutputDirectory NuGet -Prop Configuration=Release
+mkdir NuGet
 .nuget\NuGet.exe pack -sym LightBDD\LightBDD.csproj -OutputDirectory NuGet -Prop Configuration=Release
+.nuget\NuGet.exe pack -sym LightBDD.NUnit\LightBDD.NUnit.csproj -OutputDirectory NuGet -Prop Configuration=Release
+.nuget\NuGet.exe pack -sym LightBDD.MbUnit\LightBDD.MbUnit.csproj -OutputDirectory NuGet -Prop Configuration=Release
 .nuget\NuGet.exe pack LightBDD.nuspec -OutputDirectory NuGet
