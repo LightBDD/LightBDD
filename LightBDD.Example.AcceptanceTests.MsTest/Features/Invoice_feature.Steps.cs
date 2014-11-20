429802905642a3ee0486754865a34e945d4e28fb
Suremaker <wojciech.kotlarski@gmail.com>
2014-11-20 23:45:45 +0000

    cleanup

--- LightBDD.Example.AcceptanceTests.MsTest/Features/Invoice_feature.Steps.cs
+++ LightBDD.Example.AcceptanceTests.MsTest/Features/Invoice_feature.Steps.cs
@@ -1,4 +1,4 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LightBDD.Example.AcceptanceTests.MsTest.Features
 {
--- LightBDD/Execution/ParameterizedStep.cs
+++ LightBDD/Execution/ParameterizedStep.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
 using LightBDD.Assertion.Exceptions;
 using LightBDD.Execution.Parameters;
