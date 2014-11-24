c444db929e416bb1ca9351c1089a4d6b80fa59bb
Suremaker <wojciech.kotlarski@gmail.com>
2014-11-24 16:43:31 +0000

    js refactoring

--- LightBDD/Results/Formatters/Html/scripts.js
+++ LightBDD/Results/Formatters/Html/scripts.js
@@ -1,9 +1,38 @@
-﻿function checkAll(checkBoxClass, checked) {
-    var buttons = document.getElementsByTagName('input');
-    for (var i = buttons.length - 1; i >= 0; i--) {
-        if (buttons[i].classList.contains(checkBoxClass))
-            buttons[i].checked = checked;
-    }
+﻿function Queryable(array) {
+    this._source = array;
+    this._where = function (value) { return true; };
+    this.where = function (predicate) { this._where = predicate; return this; };
+    this.do = function (action) {
+        var srcLength = this._source.length;
+        for (var i = 0; i < srcLength; ++i) {
+            if (this._where(this._source[i]))
+                action(this._source[i]);
+        }
+    };
+    this.any = function () {
+        var srcLength = this._source.length;
+        for (var i = 0; i < srcLength; ++i) {
+            if (this._where(this._source[i]))
+                return true;
+        }
+        return false;
+    };
+    this.toArray = function (select) {
+        var result = [];
+        this.do(function (o) { result.push(select(o)); });
+        return result;
+    };
+}
+
+Object.prototype.asQueryable = function () {
+    return new Queryable(this);
+};
+
+function checkAll(checkBoxClass, checked) {
+    document.getElementsByTagName('input')
+        .asQueryable()
+        .where(function (b) { return b.classList.contains(checkBoxClass); })
+        .do(function (b) { b.checked = checked; });
 }
 function sortTable(tableId, columnIdx, numeric, toggle) {
     var direction = toggle.dataset.dir === 'true';
@@ -26,76 +55,58 @@ function sortTable(tableId, columnIdx, numeric, toggle) {
 
     var tbl = document.getElementById(tableId).tBodies[0];
     var store = [];
-    var i;
-    var len;
-
-    for (i = 0, len = tbl.rows.length; i < len; i++) {
-        var row = tbl.rows[i];
-        var sortnr = parseMethod(row.cells[columnIdx].textContent || row.cells[columnIdx].innerText);
-        if (checkMethod(sortnr)) store.push([sortnr, row]);
-    }
+    tbl.rows.asQueryable()
+        .do(function (row) {
+            var sortnr = parseMethod(row.cells[columnIdx].textContent || row.cells[columnIdx].innerText);
+            if (checkMethod(sortnr)) store.push([sortnr, row]);
+        });
     store.sort(sortMethod);
-    for (i = 0, len = store.length; i < len; i++) {
-        tbl.appendChild(store[i][1]);
-    }
-    store = null;
+    store.asQueryable().do(function (row) { tbl.appendChild(row[1]); });
 }
 
 function applyFilter() {
     var getFilterValues = function (elementsName) {
-        var result = [];
-        var elems = document.getElementsByName(elementsName);
-        for (var i = elems.length - 1; i >= 0; i--) {
-            if (elems[i].checked) {
-                result.push(elems[i].dataset.filterValue);
-            }
-        }
-        return result;
+        return document.getElementsByName(elementsName)
+            .asQueryable()
+            .where(function (e) { return e.checked; })
+            .toArray(function (e) { return e.dataset.filterValue; });
     };
 
     var applyTo = function (tag, className, filter) {
-        var elements = document.getElementsByTagName(tag);
-        for (var i = elements.length - 1; i >= 0; i--) {
-            if (elements[i].classList.contains(className)) {
-                filter(elements[i]);
-            }
-        }
+        document.getElementsByTagName(tag)
+            .asQueryable()
+            .where(function (e) { return e.classList.contains(className); })
+            .do(filter);
     };
 
     var statusFilterValues = getFilterValues('statusFilter');
     var categoryFilterValues = getFilterValues('categoryFilter');
 
     var statusFilter = function (scenario) {
-        for (var i = statusFilterValues.length - 1; i >= 0; i--) {
-            if (scenario.classList.contains(statusFilterValues[i]))
-                return true;
-        }
-        return false;
+        return statusFilterValues.asQueryable()
+            .where(function (s) { return scenario.classList.contains(s); })
+            .any();
     };
 
     var categoryFilter = null;
 
-    if (categoryFilterValues.length == 0 || categoryFilterValues[0] === 'all')
+    if (categoryFilterValues.length == 0 || categoryFilterValues[0] === 'all') {
         categoryFilter = function (scenario) { return true; };
-    else if (categoryFilterValues[0] === 'without')
+    } else if (categoryFilterValues[0] === 'without') {
         categoryFilter = function (scenario) { return scenario.dataset.categories == ''; };
-    else categoryFilter = function (scenario) {
-        for (var i = categoryFilterValues.length - 1; i >= 0; i--) {
-            if (scenario.dataset.categories.indexOf(categoryFilterValues[i]) >= 0)
-                return true;
-        }
-        return false;
-    };
+    } else {
+        categoryFilter = function (scenario) {
+            return categoryFilterValues.asQueryable()
+                .where(function (c) { return scenario.dataset.categories.indexOf(c) >= 0; })
+                .any();
+        };
+    }
 
     var childFilter = function (feature) {
-        var children = feature.getElementsByTagName('div');
-        for (var i = children.length - 1; i >= 0; i--) {
-            if (children[i].classList.contains('scenario')) {
-                if (children[i].dataset.filtered == 'true')
-                    return true;
-            }
-        }
-        return false;
+        return feature.getElementsByTagName('div')
+            .asQueryable()
+            .where(function (ch) { return ch.classList.contains('scenario') && ch.dataset.filtered == 'true'; })
+            .any();
     };
 
     applyTo('div', 'scenario', function (scenario) {
