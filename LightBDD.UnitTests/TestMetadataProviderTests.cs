b1556a6618e4da8834ad776c8eaa825025077f0d
Suremaker <wojciech.kotlarski@gmail.com>
2014-11-24 20:51:19 +0000

    added more tests for categories

---
+++ LightBDD.UnitTests/TestMetadataProviderTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using LightBDD.UnitTests.Helpers;
+using NUnit.Framework;
+
+namespace LightBDD.UnitTests
+{
+    [TestFixture]
+    public class TestMetadataProviderTests
+    {
+        private TestMetadataProvider _subject;
+
+        [ScenarioCategory("BaseC")]
+        class BaseClass
+        {
+            [ScenarioCategory("BaseMA")]
+            public virtual void MethodA() { }
+            [ScenarioCategory("BaseMB")]
+            public virtual void MethodB() { }
+        }
+
+        [ScenarioCategory("DerivedC")]
+        class DerivedClass : BaseClass
+        {
+            [ScenarioCategory("DerivedMB")]
+            public override void MethodB()
+            {
+            }
+
+            [ScenarioCategory("DerivedMC")]
+            public void MethodC()
+            {
+            }
+        }
+
+        #region Setup/Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            _subject = new TestableMetadataProvider();
+        }
+
+        #endregion
+
+        [Test]
+        public void Should_collect_categories_from_method_and_classes()
+        {
+            Assert.That(
+                _subject.GetScenarioCategories(typeof(DerivedClass).GetMethod("MethodC")).ToArray(),
+                Is.EqualTo(new[] { "BaseC", "DerivedC", "DerivedMC" }));
+        }
+
+        [Test]
+        public void Should_collect_categories_from_method_with_base_and_classes()
+        {
+            Assert.That(
+                _subject.GetScenarioCategories(typeof(DerivedClass).GetMethod("MethodB")).ToArray(),
+                Is.EqualTo(new[] { "BaseC","BaseMB", "DerivedC", "DerivedMB" }));
+        }
+
+        [Test]
+        public void Should_collect_categories_from_base_method_and_base_class_only_if_method_is_declared_in_base()
+        {
+            Assert.That(
+                _subject.GetScenarioCategories(typeof(DerivedClass).GetMethod("MethodA")).ToArray(),
+                Is.EqualTo(new[] { "BaseC", "BaseMA" }));
+        }
+    }
+}
