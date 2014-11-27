7ba7b8db2a27aa1a6a513007b4f0a7dc3a5d68ae
Suremaker <wojciech.kotlarski@gmail.com>
2014-11-27 20:50:44 +0000

    updated assembly version and changelog

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013-2014")]
-[assembly: AssemblyVersion("1.5.2.0")]
-[assembly: AssemblyFileVersion("1.5.2.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
