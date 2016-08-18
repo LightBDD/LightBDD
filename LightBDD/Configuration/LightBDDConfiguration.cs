1678cdac4554473a6aa4422f65c2882c7a58637d
Martin Goulet <martin.goulet@outlook.com>
2016-08-18 14:42:36 -0400

    Add a second SummaryWriter to add date in the outputPath.

--- LightBDD/Configuration/LightBDDConfiguration.cs
+++ LightBDD/Configuration/LightBDDConfiguration.cs
@@ -7,6 +7,7 @@ namespace LightBDD.Configuration
     /// </summary>
     internal class LightBDDConfiguration : ConfigurationSection
     {
+        private const string WRITER_FIELD = "writer";
         private const string SUMMARY_WRITERS_FIELD = "summaryWriters";
         private const string STEP_TYPES_FIELD = "stepTypes";
 
@@ -30,6 +31,13 @@ namespace LightBDD.Configuration
         {
             get { return (StepTypesElement)this[STEP_TYPES_FIELD]; }
             set { this[STEP_TYPES_FIELD] = value; }
+        }
+
+        [ConfigurationProperty(WRITER_FIELD)]
+        public WriterElement Writer
+        {
+            get { return (WriterElement)this[WRITER_FIELD]; }
+            set { this[WRITER_FIELD] = value; }
         }
     }
 }
