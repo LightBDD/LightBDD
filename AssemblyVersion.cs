170278a35d1a8ad9de47c70331dacbeb50af2c0b
Suremaker <wojciech.kotlarski@gmail.com>
2016-08-21 18:24:43 +0100

    Updated LightBdd version

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013-2015")]
-[assembly: AssemblyVersion("1.7.1.0")]
-[assembly: AssemblyFileVersion("1.7.1.0")]
+[assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013-2016")]
+[assembly: AssemblyVersion("1.7.2.0")]
+[assembly: AssemblyFileVersion("1.7.2.0")]
