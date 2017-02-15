2d878769068799ae9050e4a92562fc0366807562
Suremaker <wojciech.kotlarski@gmail.com>
2017-02-15 22:03:15 +0000

    Updated projects information

--- templates/ItemTemplates/LightBDD.XUnit2.FeatureTemplate/Properties/AssemblyInfo.cs
+++ templates/ItemTemplates/LightBDD.XUnit2.FeatureTemplate/Properties/AssemblyInfo.cs
@@ -1,36 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("LightBDD.XUnit2.FeatureTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Wojciech Kotlarski")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LightBDD.XUnit2.FeatureTemplate")]
-[assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8b8652d8-a72d-4d29-900d-9783bb753163")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
