6aaaca6df678d161f3d2f84e490df43735af0d0e
Suremaker <wojciech.kotlarski@gmail.com>
2017-02-26 22:50:49 +0000

    #16 Added VS ItemTemplate for LightBDD.NUnit2

---
+++ templates/ItemTemplates/LightBDD.NUnit2.FeatureTemplate/Feature.Steps.cs
@@ -0,0 +1,23 @@
+﻿using LightBDD.NUnit2;
+using NUnit.Framework;
+
+namespace $rootnamespace$
+{
+	public partial class $safeitemname$: FeatureFixture
+	{
+		private void Given_template_method()
+		{
+			Assert.Ignore("Not implemented yet");
+		}
+
+		private void When_template_method()
+		{
+			Assert.Ignore("Not implemented yet");
+		}
+
+		private void Then_template_method()
+		{
+			Assert.Ignore("Not implemented yet");
+		}
+	}
+}
