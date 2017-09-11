3fe3da1fc95d7ec764cc798216f132041d20b21c
Suremaker <wojciech.kotlarski@gmail.com>
2017-09-11 22:01:55 +0100

    #18 Added example scenarios

--- examples/LightBDD.Example.AcceptanceTests.NUnit2/Features/Customer_journey.cs
+++ examples/LightBDD.Example.AcceptanceTests.NUnit2/Features/Customer_journey.cs
@@ -9,7 +9,7 @@ namespace LightBDD.Example.AcceptanceTests.NUnit2.Features
 @"In order to receive a product
 As an application user
 I want to go through entire customer journey")]
-    [Label("Story-6")]
+    [Label("Story-7")]
     public partial class Customer_journey : FeatureFixture
     {
         [Scenario]
---
+++ examples/LightBDD.Example/Domain/Calculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LightBDD.Example.Domain
+{
+    public class Calculator
+    {
+        public int Add(int x, int y) => Math.Abs(x + y);
+        public int Multiply(int x, int y) => x * y;
+        public int Divide(int x, int y) => x / y;
+    }
+}
