8c91a076acd419afa9bc4f0b468e0eb60755ed65
Wojtek <wojciech.kotlarski@gmail.com>
2018-01-14 18:38:06 +0000

    Preparing release 2.3.4

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013-2018")]
-[assembly: AssemblyVersion("2.3.3.0")]
-[assembly: AssemblyFileVersion("2.3.3.0")]
-[assembly: AssemblyInformationalVersion("2.3.3.0")]
+[assembly: AssemblyVersion("2.3.4.0")]
+[assembly: AssemblyFileVersion("2.3.4.0")]
+[assembly: AssemblyInformationalVersion("2.3.4.0")]
