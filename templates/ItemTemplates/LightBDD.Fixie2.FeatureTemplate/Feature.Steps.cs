0235546554adc5189cfdfd0637b3177e96a97e5e
Suremaker <wojciech.kotlarski@gmail.com>
2018-05-12 11:29:30 +0100

    #67 Updated VSIX with LightBDD.Fixie2 templates

---
+++ templates/ItemTemplates/LightBDD.Fixie2.FeatureTemplate/Feature.Steps.cs
@@ -0,0 +1,23 @@
+﻿using LightBDD.Framework;
+using LightBDD.Fixie2;
+
+namespace $rootnamespace$
+{
+	public partial class $safeitemname$: FeatureFixture
+	{
+		private void Given_template_method()
+		{
+			StepExecution.Current.IgnoreScenario("Not implemented yet");
+		}
+
+		private void When_template_method()
+		{
+			StepExecution.Current.IgnoreScenario("Not implemented yet");
+		}
+
+		private void Then_template_method()
+		{
+			StepExecution.Current.IgnoreScenario("Not implemented yet");
+		}
+	}
+}
