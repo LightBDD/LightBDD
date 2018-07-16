7a12ff47a503ae1bb20cfc4156e3a0deb4ea8098
Wojtek <wojciech.kotlarski@gmail.com>
2018-07-16 22:49:18 +0100

    #122 Provided integration with DI containers implementing Microsoft DI abstractions (#125)

---
+++ src/LightBDD.Extensions.DependencyInjection/DiContainerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using LightBDD.Core.Configuration;
+using LightBDD.Extensions.DependencyInjection.Implementation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LightBDD.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension class providing integration methods for Microsoft DI compatible containers.
+    /// </summary>
+    public static class DiContainerExtensions
+    {
+        /// <summary>
+        /// Configures LightBDD to use DI container described by <paramref name="serviceProvider"/>.
+        /// Please note that the new scope will be created to handle injections for LightBDD.
+        /// </summary>
+        /// <param name="configuration">Configuration.</param>
+        /// <param name="serviceProvider">Service provider instance.</param>
+        public static DependencyContainerConfiguration UseContainer(
+            this DependencyContainerConfiguration configuration, IServiceProvider serviceProvider)
+        {
+            return configuration.UseContainer(new DiContainer(serviceProvider.CreateScope(), new ContainerOverrides()));
+        }
+    }
+}
---
+++ test/LightBDD.Extensions.DependencyInjection.UnitTests/MicrosoftDiContainer_tests.cs
@@ -0,0 +1,22 @@
+﻿using LightBDD.Core.Configuration;
+using LightBDD.Core.Dependencies;
+using LightBDD.UnitTests.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace LightBDD.Extensions.DependencyInjection.UnitTests
+{
+    [TestFixture]
+    public class MicrosoftDiContainer_tests : ContainerBaseTests
+    {
+        protected override IDependencyContainer CreateContainer()
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddTransient<Disposable>();
+
+            return new DependencyContainerConfiguration()
+                .UseContainer(serviceCollection.BuildServiceProvider())
+                .DependencyContainer;
+        }
+    }
+}
