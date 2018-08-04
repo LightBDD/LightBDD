baa48aef93fcc4d8b21c6be152d114ab65f4220a
Suremaker <wojciech.kotlarski@gmail.com>
2018-08-04 15:09:31 +0100

    Added State<T> helper struct managing uninitialized shared state fields

---
+++ src/LightBDD.Framework/State.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace LightBDD.Framework
+{
+    /// <summary>
+    /// A helper struct designed to protect scenario state fields from being accessed without former initialization.
+    /// </summary>
+    public struct State<T>
+    {
+        private readonly T _value;
+        /// <summary>
+        /// Returns true if state is initialized, otherwise false.
+        /// The default instance of <see cref="State{T}"/> type represents uninitialized state.
+        /// </summary>
+        public bool IsInitialized { get; }
+
+        /// <summary>
+        /// Creates state instance, initialized with <paramref name="value"/> value.
+        /// The <see cref="IsInitialized"/> is always set to true, even if <paramref name="value"/> itself is null.
+        /// The <paramref name="value"/> is retrievable with <see cref="GetValue"/> method or implicit cast.
+        /// </summary>
+        /// <param name="value"></param>
+        public State(T value)
+        {
+            _value = value;
+            IsInitialized = true;
+        }
+
+        /// <summary>
+        /// Returns the held value, or throws <see cref="InvalidOperationException"/> if state is not initialized.
+        /// </summary>
+        /// <param name="memberName">If specified, the member name will be used in exception. If not specified, the state type will be used.</param>
+        /// <exception cref="InvalidOperationException">Thrown if value is not initialized.</exception>
+        public T GetValue(string memberName = null)
+        {
+            if (IsInitialized)
+                return _value;
+            throw new InvalidOperationException($"The {memberName ?? typeof(T).Name} state is not initialized.");
+        }
+
+        /// <summary>
+        /// Returns the held value or <paramref name="defaultValue"/> if state is not initialized or held value is null (in case where <typeparam name="T"></typeparam> is reference type).
+        /// </summary>
+        public T GetValueOrDefault(T defaultValue = default(T))
+        {
+            if (!IsInitialized)
+                return defaultValue;
+            return _value != null ? _value : defaultValue;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return IsInitialized ? _value?.ToString() : "<not set>";
+        }
+
+        /// <summary>
+        /// Implicit cast, converting <paramref name="value"/> to initialized state instance.
+        /// </summary>
+        public static implicit operator State<T>(T value) => new State<T>(value);
+        /// <summary>
+        /// Implicit cast, retrieving value of <paramref name="state"/> or throwing <see cref="InvalidOperationException"/> if state is not initialized.
+        /// </summary>
+        /// <param name="state"></param>
+        public static implicit operator T(State<T> state) => state.GetValue();
+    }
+}
---
+++ test/LightBDD.Framework.UnitTests/State_tests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LightBDD.Framework.UnitTests
+{
+    [TestFixture]
+    public class State_tests
+    {
+        [Test]
+        public void State_should_be_uninitialized_by_default()
+        {
+            State<int> state = default;
+            Assert.False(state.IsInitialized);
+            Assert.False(new State<bool>().IsInitialized);
+        }
+
+        [Test]
+        public void It_should_be_possible_to_set_and_retrieve_state()
+        {
+            var state = new State<int>();
+            Assert.False(state.IsInitialized);
+
+            state = 0;
+            AssertInitializedValue(state, 0);
+
+            state = new State<int>(5);
+            AssertInitializedValue(state, 5);
+
+            int x = state;
+            Assert.That(x, Is.EqualTo(5));
+
+            var state2 = new State<string>("text");
+            AssertInitializedValue(state2, "text");
+
+            state2 = null;
+            AssertInitializedValue(state2, null);
+
+            string s = state2;
+            Assert.That(s, Is.Null);
+        }
+
+        [Test]
+        public void GetValue_should_throw_if_state_is_not_initialized()
+        {
+            State<bool> state = default;
+
+            var ex = Assert.Throws<InvalidOperationException>(() => state.GetValue());
+            Assert.That(ex.Message, Is.EqualTo("The Boolean state is not initialized."));
+
+            ex = Assert.Throws<InvalidOperationException>(() =>
+            {
+                bool x = state;
+            });
+            Assert.That(ex.Message, Is.EqualTo("The Boolean state is not initialized."));
+
+            ex = Assert.Throws<InvalidOperationException>(() => state.GetValue("myField"));
+            Assert.That(ex.Message, Is.EqualTo("The myField state is not initialized."));
+        }
+
+        [Test]
+        public void GetValueOrDefault_should_return_value_if_initialized_or_default_if_not_or_value_is_null()
+        {
+            var defaultInt = 5;
+            var defaultString = "default";
+
+            State<int> state = default;
+            Assert.AreEqual(defaultInt, state.GetValueOrDefault(defaultInt));
+            state = 0;
+            Assert.AreEqual(0, state.GetValueOrDefault(defaultInt));
+
+            State<string> state2 = default;
+            Assert.AreEqual(defaultString, state2.GetValueOrDefault(defaultString));
+
+            state2 = "value";
+            Assert.AreEqual("value", state2.GetValueOrDefault(defaultString));
+
+            state2 = null;
+            Assert.AreEqual(defaultString, state2.GetValueOrDefault(defaultString));
+        }
+
+        private static void AssertInitializedValue<T>(State<T> state, T expected)
+        {
+            Assert.True(state.IsInitialized);
+            Assert.AreEqual(expected, state.GetValue());
+        }
+    }
+}
