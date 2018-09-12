29b368a4cd0419c96ef02621bce0050d306e0a7d
Suremaker <wojciech.kotlarski@gmail.com>
2018-09-12 22:35:51 +0100

    Prepared 2.4.3 release

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013-2018")]
-[assembly: AssemblyVersion("2.4.2")]
-[assembly: AssemblyFileVersion("2.4.2")]
-[assembly: AssemblyInformationalVersion("2.4.2")]
+[assembly: AssemblyVersion("2.4.3")]
+[assembly: AssemblyFileVersion("2.4.3")]
+[assembly: AssemblyInformationalVersion("2.4.3")]
