5663ca4f70b23b152afcd367a950f7d7005ef827
Lars Wuckel <wuckel@hhk.de>
2018-12-20 17:32:32 +0100

    einfacher Test

---
+++ test/LightBDD.Framework.UnitTests/Scenarios/Fluent/Fluent_Scene_tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LightBDD.Core.Extensibility;
+using LightBDD.Framework.Scenarios.Basic;
+using LightBDD.Framework.Scenarios.Fluent;
+using LightBDD.Framework.UnitTests.Scenarios.Basic.Helpers;
+using Moq;
+using NUnit.Framework;
+
+namespace LightBDD.Framework.UnitTests.Scenarios.Fluent
+{
+	[TestFixture]
+	public class Fluent_Scene_tests 
+	{
+
+		[Test]
+		public void Runner_should_accept_Fluent_Api()
+		{
+			int given = 0;
+			int when = 0;
+			int then = 0;
+
+			var actions =
+				Scene<Action>
+				.Given(() => ++given)
+				.And(() => ++given)
+				.When(() => ++when)
+				.And(() => ++when)
+				.Then(() => ++then)
+				.And(() => ++then)
+				.End();
+
+			foreach (var action in actions)
+				action();
+
+			Assert.That(actions.Length, Is.EqualTo(6));
+			Assert.That(given, Is.EqualTo(2));
+			Assert.That(when, Is.EqualTo(2));
+			Assert.That(then, Is.EqualTo(2));
+		}
+	}
+}
