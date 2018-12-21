ed867594414462b2c18005cf14b70a5fc40e6bbc
Lars Wuckel <wuckel@hhk.de>
2018-12-21 10:33:18 +0100

    Test korrigiert

--- test/LightBDD.Framework.UnitTests/Scenarios/Fluent/Fluent_Scene_tests.cs
+++ test/LightBDD.Framework.UnitTests/Scenarios/Fluent/Fluent_Scene_tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using LightBDD.Core.Extensibility;
 using LightBDD.Framework.Scenarios.Basic;
 using LightBDD.Framework.Scenarios.Fluent;
@@ -15,7 +16,7 @@ namespace LightBDD.Framework.UnitTests.Scenarios.Fluent
 	{
 
 		[Test]
-		public void Runner_should_accept_Fluent_Api()
+		public void FluentApi_should_generate_array_of_steps()
 		{
 			int given = 0;
 			int when = 0;
@@ -33,6 +34,36 @@ namespace LightBDD.Framework.UnitTests.Scenarios.Fluent
 
 			foreach (var action in actions)
 				action();
+
+			Assert.That(actions.Length, Is.EqualTo(6));
+			Assert.That(given, Is.EqualTo(2));
+			Assert.That(when, Is.EqualTo(2));
+			Assert.That(then, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void FluentApi_should_generate_array_of_steps_async()
+		{
+			int given = 0;
+			int when = 0;
+			int then = 0;
+
+			var actions =
+				SceneAsync
+				.Given(() => new Task(() => ++given))
+				.And(() => new Task(() => ++given))
+				.When(() => new Task(() => ++when))
+				.And(() => new Task(() => ++when))
+				.Then(() => new Task(() => ++then))
+				.And(() => new Task(() => ++then))
+				.End();
+
+			foreach (var action in actions)
+			{
+				var task = action();
+				task.Start();
+				task.Wait();
+			}
 
 			Assert.That(actions.Length, Is.EqualTo(6));
 			Assert.That(given, Is.EqualTo(2));
