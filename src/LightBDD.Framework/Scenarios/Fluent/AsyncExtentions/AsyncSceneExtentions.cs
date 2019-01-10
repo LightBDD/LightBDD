3a60a14c2fa3b66a178b329482ca5b600eae9c1d
Lars Wuckel <wuckel@hhk.de>
2019-01-10 15:20:24 +0100

    Action and Func Extentions are now separate because of Error CS0121

---
+++ src/LightBDD.Framework/Scenarios/Fluent/AsyncExtentions/AsyncSceneExtentions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using LightBDD.Framework.Scenarios.Basic;
+
+namespace LightBDD.Framework.Scenarios.Fluent
+{
+	/// <summary>
+	/// Extention methods for Given-When-Then
+	/// </summary>
+	public static class AsyncSceneExtentions
+	{
+		/// <summary>
+		/// Given Extention for IBddRunner 
+		/// </summary>
+		/// <param name="runner">Test-Runner</param>
+		/// <param name="given">Test-Func to be executed</param>
+		/// <returns>Given-object</returns>
+		public static IGiven<Func<Task>> Given(this IBddRunner runner, Func<Task> given)
+		{
+			return new Scene<Func<Task>>(runner)
+				.Given(given);
+		}
+
+		/// <summary>
+		/// Run async the definition of the SceneContext.
+		/// <see cref="SceneContext{TGiven, TWhen, TThen, TFunc_Given}"/>
+		/// Execute the steps
+		/// </summary>
+		/// <typeparam name="TThen">Type of then-methods class</typeparam>
+		/// <param name="then">ThenResult object</param>
+		public static Task RunAsync<TThen>(this ThenResult<TThen, Func<Task>> then)
+		{
+			var scene = (Scene<Func<Task>>)then.To();
+			return scene.Runner.RunScenarioAsync(scene.End());
+		}
+
+		/// <summary>
+		/// Run Async the definition of the Scene
+		/// <see cref="Scene{TAction}"/>
+		/// </summary>
+		/// <param name="then">Then-object</param>
+		public static Task RunAsync(this IThen<Func<Task>> then) 
+		{
+			var scene = (Scene<Func<Task>>)then;
+			return scene.Runner.RunScenarioAsync(scene.End());
+		}
+	}
+}
--- src/LightBDD.Framework/Scenarios/Fluent/SceneExtentions.cs
+++ src/LightBDD.Framework/Scenarios/Fluent/SceneExtentions.cs
@@ -24,18 +24,6 @@ namespace LightBDD.Framework.Scenarios.Fluent
 		}
 
 		/// <summary>
-		/// Given Extention for IBddRunner 
-		/// </summary>
-		/// <param name="runner">Test-Runner</param>
-		/// <param name="given">Test-Func to be executed</param>
-		/// <returns>Given-object</returns>
-		public static IGiven<Func<Task>> Given(this IBddRunner runner, Func<Task> given)
-		{
-			return new Scene<Func<Task>>(runner)
-				.Given(given);
-		}
-
-		/// <summary>
 		/// Run the definition of the SceneContext.
 		/// <see cref="SceneContext{TGiven, TWhen, TThen, TFunc_Given}"/>
 		/// Execute the steps
@@ -57,30 +45,6 @@ namespace LightBDD.Framework.Scenarios.Fluent
 		{
 			var scene = (Scene<Action>)then;
 			scene.Runner.RunScenario(scene.End());
-		}
-
-		/// <summary>
-		/// Run async the definition of the SceneContext.
-		/// <see cref="SceneContext{TGiven, TWhen, TThen, TFunc_Given}"/>
-		/// Execute the steps
-		/// </summary>
-		/// <typeparam name="TThen">Type of then-methods class</typeparam>
-		/// <param name="then">ThenResult object</param>
-		public static Task RunAsync<TThen>(this ThenResult<TThen, Func<Task>> then)
-		{
-			var scene = (Scene<Func<Task>>)then.To();
-			return scene.Runner.RunScenarioAsync(scene.End());
-		}
-
-		/// <summary>
-		/// Run Async the definition of the Scene
-		/// <see cref="Scene{TAction}"/>
-		/// </summary>
-		/// <param name="then">Then-object</param>
-		public static Task RunAsync(this IThen<Func<Task>> then) 
-		{
-			var scene = (Scene<Func<Task>>)then;
-			return scene.Runner.RunScenarioAsync(scene.End());
 		}
 
 		/// <summary>
