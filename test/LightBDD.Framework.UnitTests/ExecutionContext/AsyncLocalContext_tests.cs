6274df6c43caa724358ea9183f1c2f4e425a9b05
suremaker <wojciech.kotlarski@gmail.com>
2020-05-24 13:51:12 +0100

    Decreased test execution time

--- test/LightBDD.Framework.UnitTests/ExecutionContext/AsyncLocalContext_tests.cs
+++ test/LightBDD.Framework.UnitTests/ExecutionContext/AsyncLocalContext_tests.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 namespace LightBDD.Framework.UnitTests.ExecutionContext
 {
     [TestFixture]
+    [Parallelizable(ParallelScope.All)]
     public class AsyncLocalContext_tests
     {
         [Test]
