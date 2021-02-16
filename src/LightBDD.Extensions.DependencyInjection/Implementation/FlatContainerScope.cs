8e512a66c9db860228592eaaef65545992755b71
suremaker <wojciech.kotlarski@gmail.com>
2021-02-16 23:34:42 +0000

    Updated DependencyInjection integration to share container scope between scenario and composite steps by default and made it configurable with DiContainerOptions.EnableScopeNestingWithinScenarios()

---
+++ src/LightBDD.Extensions.DependencyInjection/Implementation/FlatContainerScope.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LightBDD.Core.Dependencies;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LightBDD.Extensions.DependencyInjection.Implementation
+{
+    internal class FlatContainerScope : IContainerScope
+    {
+        private readonly IServiceScope _serviceScope;
+        private readonly bool _externallyOwned;
+
+        public FlatContainerScope(IServiceScope serviceScope, bool externallyOwned)
+        {
+            _serviceScope = serviceScope;
+            _externallyOwned = externallyOwned;
+        }
+
+        public void Dispose()
+        {
+            if (!_externallyOwned)
+                _serviceScope.Dispose();
+        }
+
+        public object Resolve(Type type) => _serviceScope.ServiceProvider.GetRequiredService(type);
+
+        public IContainerScope BeginScope(LifetimeScope scope) => new FlatContainerScope(_serviceScope, true);
+    }
+}
