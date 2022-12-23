8ab268c9db35467d750efdf3eb91bdfcdaffd2b5
suremaker <wojciech.kotlarski@gmail.com>
2022-12-23 15:25:28 +0000

    Initial implementation of ObjectTreeBuilder

---
+++ src/LightBDD.Framework/Parameters/ObjectTreeBuilder.cs
@@ -0,0 +1,118 @@
+﻿#nullable enable
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LightBDD.Framework.Parameters;
+
+public class ObjectTreeBuilderOptions
+{
+    public HashSet<Type> ValueTypes { get; } = new()
+    {
+        typeof(string),
+        typeof(IFormattable)
+    };
+
+}
+public class ObjectTreeBuilder
+{
+    private readonly ObjectTreeBuilderOptions _options;
+
+    public ObjectTreeBuilder(ObjectTreeBuilderOptions options)
+    {
+        _options = options;
+    }
+
+    public ObjectTreeNode Build(object? o) => Build(o, CombinePath(null));
+
+    private ObjectTreeNode Build(object? o, string path)
+    {
+        if (o is null)
+            return new ObjectTreeValue(path, o);
+
+        var type = o.GetType();
+        if (type.IsPrimitive || _options.ValueTypes.Any(t => t.IsAssignableFrom(type)))
+            return new ObjectTreeValue(path, o);
+
+        if (o is IEnumerable collection)
+            return CreateArray(collection, path);
+
+        return CreateObject(o, type, path);
+    }
+
+    private ObjectTreeNode CreateObject(object o, Type type, string path)
+    {
+        var props = type.GetFields().Select(f => new KeyValuePair<string, object?>(f.Name, f.GetValue(o)))
+            .Concat(type.GetProperties().Select(p => new KeyValuePair<string, object?>(p.Name, p.GetValue(o))))
+            .ToDictionary(x => x.Key, x => Build(x.Value, CombinePath(path, $".{x.Key}")));
+        return new ObjectTreeObject(path, props);
+    }
+
+    private ObjectTreeArray CreateArray(IEnumerable collection, string path)
+    {
+        var items = collection.Cast<object>().Select((o, i) => Build(o, CombinePath(path, $"[{i}]")));
+        return new ObjectTreeArray(path, items);
+    }
+
+    private static string CombinePath(string? parent, string? node = null)
+    {
+        parent ??= "$";
+        return string.IsNullOrWhiteSpace(node)
+            ? parent
+            : $"{parent}{node}";
+    }
+}
+
+public abstract class ObjectTreeNode
+{
+    public string Path { get; }
+    public abstract ObjectTreeNodeKind Kind { get; }
+
+    protected ObjectTreeNode(string path)
+    {
+        Path = path;
+    }
+
+    public ObjectTreeValue AsValue() => this as ObjectTreeValue ?? throw new InvalidOperationException($"{GetType().Name} node at {Path} path is not {nameof(ObjectTreeValue)}");
+    public ObjectTreeArray AsArray() => this as ObjectTreeArray ?? throw new InvalidOperationException($"{GetType().Name} node at {Path} path is not {nameof(ObjectTreeArray)}");
+    public ObjectTreeObject AsObject() => this as ObjectTreeObject ?? throw new InvalidOperationException($"{GetType().Name} node at {Path} path is not {nameof(ObjectTreeObject)}");
+}
+
+public class ObjectTreeObject : ObjectTreeNode
+{
+    public ObjectTreeObject(string path, IReadOnlyDictionary<string, ObjectTreeNode> properties) : base(path)
+    {
+        Properties = properties;
+    }
+
+    public override ObjectTreeNodeKind Kind => ObjectTreeNodeKind.Object;
+    public IReadOnlyDictionary<string, ObjectTreeNode> Properties { get; }
+}
+
+public class ObjectTreeArray : ObjectTreeNode
+{
+    public ObjectTreeArray(string path, IEnumerable<ObjectTreeNode> items) : base(path)
+    {
+        Items = items.ToArray();
+    }
+
+    public override ObjectTreeNodeKind Kind => ObjectTreeNodeKind.Array;
+    public IReadOnlyList<ObjectTreeNode> Items { get; }
+}
+
+public class ObjectTreeValue : ObjectTreeNode
+{
+    public ObjectTreeValue(string path, object? value) : base(path)
+    {
+        Value = value;
+    }
+
+    public override ObjectTreeNodeKind Kind => ObjectTreeNodeKind.Value;
+    public object? Value { get; }
+}
+
+public enum ObjectTreeNodeKind
+{
+    Value, Array, Object
+}
---
+++ test/LightBDD.Framework.UnitTests/Parameters/ObjectTreeBuilder_tests.cs
@@ -0,0 +1,120 @@
+﻿#nullable enable
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using LightBDD.Framework.Parameters;
+using NUnit.Framework;
+using Shouldly;
+
+namespace LightBDD.Framework.UnitTests.Parameters
+{
+    [TestFixture, FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+    public class ObjectTreeBuilder_tests
+    {
+        [Test]
+        public void It_should_create_root_node()
+        {
+            var builder = new ObjectTreeBuilder(new());
+            var root = builder.Build(null);
+
+            AssertValueNode(root, "$", null);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetValueInstances))]
+        public void It_should_create_value_nodes_for_nulls_primitives_and_registered_types(object input)
+        {
+            var builder = new ObjectTreeBuilder(new());
+            var root = builder.Build(input);
+
+            AssertValueNode(root, "$", input);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetFlatCollectionInstances))]
+        public void It_should_create_array_node_for_collections(IEnumerable input)
+        {
+            var builder = new ObjectTreeBuilder(new());
+            var root = builder.Build(input);
+            var expectations = input.Cast<object>().ToArray();
+
+            root.Kind.ShouldBe(ObjectTreeNodeKind.Array);
+            root.Path.ShouldBe("$");
+
+            var items = root.AsArray().Items;
+            for (var i = 0; i < items.Count; ++i)
+                AssertValueNode(items[i], $"$[{i}]", expectations[i]);
+        }
+
+        [Test]
+        public void It_should_create_object_node_for_poco_type()
+        {
+            var builder = new ObjectTreeBuilder(new());
+            var input = new
+            {
+                Name = "text",
+                Number = 34,
+                Flag = DateTimeKind.Utc,
+                Value = (object?)null
+            };
+            var root = builder.Build(input);
+
+            root.Kind.ShouldBe(ObjectTreeNodeKind.Object);
+            var properties = root.AsObject().Properties;
+            AssertValueNode(properties["Name"], "$.Name", "text");
+            AssertValueNode(properties["Number"], "$.Number", 34);
+            AssertValueNode(properties["Flag"], "$.Flag", DateTimeKind.Utc);
+            AssertValueNode(properties["Value"], "$.Value", null);
+        }
+
+        private void AssertValueNode(ObjectTreeNode node, string path, object? value)
+        {
+            node.Kind.ShouldBe(ObjectTreeNodeKind.Value);
+            node.Path.ShouldBe(path);
+            node.AsValue().Value.ShouldBe(value);
+        }
+
+        public static IEnumerable<object?> GetValueInstances()
+        {
+            yield return null;
+            yield return true;
+            yield return (byte)1;
+            yield return (short)2;
+            yield return 3;
+            yield return (long)3;
+            yield return 3.14f;
+            yield return 3.14;
+            yield return "text";
+            yield return 'c';
+
+            yield return 3.77m;
+            yield return Guid.NewGuid();
+            yield return DateTime.Now;
+            yield return DateTimeOffset.Now;
+            yield return TimeSpan.FromSeconds(157);
+            yield return DateTimeKind.Utc;
+            yield return new FormattableType(5);
+        }
+
+        public static IEnumerable<IEnumerable> GetFlatCollectionInstances()
+        {
+            yield return new[] { "a", "ab", "abc" };
+            yield return new List<TimeSpan> { TimeSpan.FromSeconds(1), TimeSpan.FromHours(2) };
+            yield return Enumerable.Range(3, 7);
+        }
+    }
+
+    class FormattableType : IFormattable
+    {
+        private readonly int _value;
+
+        public FormattableType(int value)
+        {
+            _value = value;
+        }
+
+        public string ToString(string? format, IFormatProvider? formatProvider) => string.Format(formatProvider, format ?? "G", _value);
+        public override string ToString() => ToString(null, null);
+    }
+}
