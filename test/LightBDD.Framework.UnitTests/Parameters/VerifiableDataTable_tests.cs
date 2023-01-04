e74da36c8aad439cc3e7291a2c4b1a8bf27d7ee2
suremaker <wojciech.kotlarski@gmail.com>
2023-01-04 21:51:16 +0000

    Updated tests

--- test/LightBDD.Framework.UnitTests/Parameters/VerifiableDataTable_tests.cs
+++ test/LightBDD.Framework.UnitTests/Parameters/VerifiableDataTable_tests.cs
@@ -701,7 +701,7 @@ namespace LightBDD.Framework.UnitTests.Parameters
         [Test]
         [TestCase(0)]
         [TestCase(2)]
-        public void It_should_have_type_column_for_collection_of_empty_expando_objects(int count)
+        public void It_should_has_item_column_for_collection_of_empty_expando_objects_and_verify_them(int count)
         {
             var input = Enumerable.Range(0, count).Select(_ => new ExpandoObject()).ToArray();
             var table = input.ToVerifiableDataTable();
@@ -718,7 +718,7 @@ namespace LightBDD.Framework.UnitTests.Parameters
         [Test]
         [TestCase(0)]
         [TestCase(2)]
-        public void It_should_have_type_column_for_collection_of_empty_objects(int count)
+        public void It_should_has_item_column_for_collection_of_empty_objects_and_verify_them(int count)
         {
             var input = Enumerable.Range(0, count).Select(_ => new EmptyObject()).ToArray();
             var table = input.ToVerifiableDataTable();
@@ -733,7 +733,7 @@ namespace LightBDD.Framework.UnitTests.Parameters
         }
 
         [Test]
-        public void It_should_fail_if_non_empty_collection_is_provided_when_expecting_empty()
+        public void It_should_fail_if_non_empty_collection_is_provided_when_expected_empty()
         {
             var input = Array.Empty<object>();
             var table = input.ToVerifiableDataTable();
