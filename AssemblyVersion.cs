c4cc174940207425e2bcf55646526f852d26ea3a
suremaker <wojciech.kotlarski@gmail.com>
2023-01-05 23:36:51 +0000

    Updated version, copyright year, examples and project templates

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013-2021")]
-[assembly: AssemblyVersion("3.5.0")]
-[assembly: AssemblyFileVersion("3.5.0")]
-[assembly: AssemblyInformationalVersion("3.5.0")]
+[assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013-2023")]
+[assembly: AssemblyVersion("3.6.0")]
+[assembly: AssemblyFileVersion("3.6.0")]
+[assembly: AssemblyInformationalVersion("3.6.0")]
