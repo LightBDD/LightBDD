6c2d0235bf531c3d3fdf3773aff2519394ef1dcc
suremaker <wojciech.kotlarski@gmail.com>
2023-06-30 22:03:32 +0100

    Updated version

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013-2023")]
-[assembly: AssemblyVersion("3.6.1")]
-[assembly: AssemblyFileVersion("3.6.1")]
-[assembly: AssemblyInformationalVersion("3.6.1")]
+[assembly: AssemblyVersion("3.7.0")]
+[assembly: AssemblyFileVersion("3.7.0")]
+[assembly: AssemblyInformationalVersion("3.7.0")]
