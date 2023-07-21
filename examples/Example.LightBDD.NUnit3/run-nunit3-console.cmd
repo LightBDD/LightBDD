bbbcb572b6a4f8ee51c242fda9fbafea9591e795
suremaker <wojciech.kotlarski@gmail.com>
2023-07-21 16:57:18 +0100

    #309 Removed integrations not based on LightBDD.Runner

--- examples/Example.LightBDD.NUnit3/run-nunit3-console.cmd
+++
@@ -1,7 +0,0 @@
-dotnet build
-..\..\.nuget\nuget.exe install NUnit.ConsoleRunner -Version 3.16.3 -NonInteractive -ExcludeVersion -Output packages
-
-@echo.
-@echo Running tests
-
-packages\NUnit.ConsoleRunner\tools\nunit3-console.exe bin\Debug\net48\Example.LightBDD.NUnit3.dll
