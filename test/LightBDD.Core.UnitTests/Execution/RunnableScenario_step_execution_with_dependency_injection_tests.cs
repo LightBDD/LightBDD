1354d04c106ca4170886baf31d9fc561d4b6e643
suremaker <wojciech.kotlarski@gmail.com>
2023-08-09 11:54:01 +0100

    #323 Reimplemented tests for RunnableScenario and DI

--- test/LightBDD.Core.UnitTests/Execution/RunnableScenario_step_execution_with_dependency_injection_tests.cs
+++ test/LightBDD.Core.UnitTests/Execution/RunnableScenario_step_execution_with_dependency_injection_tests.cs
@@ -1,11 +1,13 @@
-﻿using System.Threading.Tasks;
-using LightBDD.Core.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using LightBDD.Core.Dependencies;
+using LightBDD.Core.ExecutionContext;
 using LightBDD.Core.Extensibility;
 using LightBDD.Core.Results;
 using LightBDD.Core.UnitTests.Helpers;
 using LightBDD.ScenarioHelpers;
-using Moq;
 using NUnit.Framework;
 using Shouldly;
 
@@ -13,65 +15,98 @@ namespace LightBDD.Core.UnitTests.Execution
 {
     [TestFixture]
     [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
-    //TODO: simplify scopes
     public class RunnableScenario_step_execution_with_dependency_injection_tests
     {
-        private readonly Mock<IDependencyContainer> _containerScope;
-        private readonly Mock<IDependencyContainer> _scenarioScope;
-        private readonly TestableScenarioFactory _factory;
-
-        public RunnableScenario_step_execution_with_dependency_injection_tests()
-        {
-            _containerScope = new Mock<IDependencyContainer>();
-            _scenarioScope = new Mock<IDependencyContainer>();
-
-            _containerScope.Setup(x => x.BeginScope())
-                .Returns(_scenarioScope.Object);
-            Assert.Ignore("To be rewritten");
-            //_factory = TestableScenarioFactory.Create(cfg => cfg.DependencyContainerConfiguration().UseContainer(_containerScope.Object));
-        }
 
         [Test]
         public async Task Runner_should_instantiate_scenario_context_within_scenario_scope()
         {
-            var result = await _factory.RunScenario(x => x.Test()
-                .WithContext(r => r.Resolve<MyScenarioScope>())
-                .TestScenario(Given_step_one));
+            MyScenarioContext captured = null;
+
+            var result = await TestableScenarioFactory.Default.RunScenario(x => x.Test()
+                .WithContext(r => captured = r.Resolve<MyScenarioContext>().VerifyNotDisposed())
+                .TestScenario(Scenario_step));
             result.Status.ShouldBe(ExecutionStatus.Passed);
 
-            _containerScope.Verify(s => s.BeginScope());
-            _scenarioScope.Verify(s => s.Resolve(typeof(MyScenarioScope)));
+            captured.ShouldNotBeNull();
+            captured.Disposed.ShouldBeTrue();
         }
 
         [Test]
         public async Task Runner_should_instantiate_step_context_within_scenario_scope()
         {
-            var result = await _factory.RunScenario(x => x.Test()
-                .WithContext(r => r.Resolve<MyScenarioScope>())
+            MyScenarioContext captured = null;
+            var result = await TestableScenarioFactory.Default.RunScenario(x => x.Test()
+                .WithContext(r => captured = r.Resolve<MyScenarioContext>().VerifyNotDisposed())
                 .TestGroupScenario(Given_composite, Given_composite));
             result.Status.ShouldBe(ExecutionStatus.Passed);
 
-            _containerScope.Verify(s => s.BeginScope(), Times.Once);
-            _scenarioScope.Verify(s => s.Resolve(typeof(MyStepScope)), Times.Exactly(2));
+            captured.ShouldNotBeNull();
+            captured.Disposed.ShouldBeTrue();
+            captured.StepContexts.Distinct().Count().ShouldBe(2);
+            captured.StepContexts.ShouldAllBe(s => s.Disposed);
         }
 
-        class MyScenarioScope
+        class MyScenarioContext : IAsyncDisposable
         {
+            public readonly List<MyStepContext> StepContexts = new();
+
+            public MyScenarioContext VerifyNotDisposed()
+            {
+                if (Disposed)
+                    throw new Exception("Already disposed");
+                return this;
+            }
+
+            public ValueTask DisposeAsync()
+            {
+                Disposed = true;
+                return default;
+            }
+
+            public bool Disposed { get; private set; }
+            
+            public MyStepContext CaptureStepContext(MyStepContext step)
+            {
+                StepContexts.Add(step);
+                return step;
+            }
         }
 
-        class MyStepScope
+        class MyStepContext : IAsyncDisposable
         {
+            public MyStepContext VerifyNotDisposed()
+            {
+                if (Disposed)
+                    throw new Exception("Already disposed");
+                return this;
+            }
+
+            public ValueTask DisposeAsync()
+            {
+                Disposed = true;
+                return default;
+            }
+
+            public bool Disposed { get; private set; }
         }
 
-        private void Given_step_one()
+        private void Step_step()
         {
+            ((MyStepContext)ScenarioExecutionContext.CurrentStep.Context).VerifyNotDisposed();
+        }
+
+        private void Scenario_step()
+        {
+            ((MyScenarioContext)ScenarioExecutionContext.CurrentStep.Context).VerifyNotDisposed();
         }
 
         private TestCompositeStep Given_composite()
         {
+            var scenarioContext = (MyScenarioContext)ScenarioExecutionContext.CurrentStep.Context;
             return new TestCompositeStep(
-                new ExecutionContextDescriptor(r => r.Resolve<MyStepScope>()),
-                TestStep.CreateSync(Given_step_one));
+                new ExecutionContextDescriptor(r => scenarioContext.CaptureStepContext(r.Resolve<MyStepContext>().VerifyNotDisposed())),
+                TestStep.CreateSync(Step_step));
         }
     }
 }
