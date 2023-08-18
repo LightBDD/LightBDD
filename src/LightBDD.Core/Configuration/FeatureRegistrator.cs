6a168944119a0ac95d783daa5d424105085c01df
suremaker <wojciech.kotlarski@gmail.com>
2023-08-18 17:09:12 +0100

    #325 limited FeatureRegistrator to register singletons

--- src/LightBDD.Core/Configuration/FeatureRegistrator.cs
+++ src/LightBDD.Core/Configuration/FeatureRegistrator.cs
@@ -4,27 +4,51 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace LightBDD.Core.Configuration;
 
+/// <summary>
+/// Feature registrator allowing to select implementation of the <typeparamref name="TService"/> feature.<br/>
+/// As features are used across whole execution cycle, they are registered as singletons.
+/// </summary>
+/// <typeparam name="TService"></typeparam>
 public class FeatureRegistrator<TService> where TService : class
 {
-    private ServiceDescriptor _featureDescriptor;
+    private ServiceDescriptor? _featureDescriptor;
 
-    public FeatureRegistrator<TService> Use<TImplementation>(ServiceLifetime lifetime = ServiceLifetime.Singleton) where TImplementation : TService
+    /// <summary>
+    /// Uses <typeparamref name="TImplementation"/> as the implementation for the <typeparamref name="TService"/> feature, using DI to resolve and control the singleton instance lifetime.
+    /// </summary>
+    /// <typeparam name="TImplementation">Implementation type to use</typeparam>
+    /// <returns>Self.</returns>
+    public FeatureRegistrator<TService> Use<TImplementation>() where TImplementation : TService
     {
-        _featureDescriptor = ServiceDescriptor.Describe(typeof(TService), typeof(TImplementation), lifetime);
+        _featureDescriptor = ServiceDescriptor.Describe(typeof(TService), typeof(TImplementation), ServiceLifetime.Singleton);
         return this;
     }
 
+    /// <summary>
+    /// Uses <paramref name="instance"/> as the implementation for the <typeparamref name="TService"/> feature, where the instance lifetime is controlled outside of DI.
+    /// </summary>
+    /// <param name="instance">Instance to use</param>
+    /// <returns>Self.</returns>
     public FeatureRegistrator<TService> Use(TService instance)
     {
         _featureDescriptor = ServiceDescriptor.Singleton(instance);
         return this;
     }
 
-    public FeatureRegistrator<TService> Use(Func<IServiceProvider, TService> implementationFactory, ServiceLifetime lifetime = ServiceLifetime.Singleton)
+    /// <summary>
+    /// Uses <paramref name="implementationFactory"/> to create the implementation for the <typeparamref name="TService"/> feature, using DI to control the singleton instance lifetime.
+    /// </summary>
+    /// <param name="implementationFactory">Implementation factory</param>
+    /// <returns>Self.</returns>
+    public FeatureRegistrator<TService> Use(Func<IServiceProvider, TService> implementationFactory)
     {
-        _featureDescriptor = ServiceDescriptor.Describe(typeof(TService), implementationFactory, lifetime);
+        _featureDescriptor = ServiceDescriptor.Describe(typeof(TService), implementationFactory, ServiceLifetime.Singleton);
         return this;
     }
 
+    /// <summary>
+    /// Returns <see cref="ServiceDescriptor"/> representing the registration.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown if no implementation was configured with Use() methods./></exception>
     public ServiceDescriptor GetDescriptor() => _featureDescriptor ?? throw new InvalidOperationException($"{nameof(_featureDescriptor)} is not configured. Configure it with one of {nameof(Use)}() methods");
 }
