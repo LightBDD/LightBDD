f5dc757a91deada083ea82177152806dc375f9cc
Sam Goldmann <sam.goldmann@gmail.com>
2024-02-18 20:21:31 -0500

    Added dependency resolver generic extension overload

---
+++ src/LightBDD.Core/Dependencies/IDependencyResolverExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace LightBDD.Core.Dependencies
+{
+    /// <summary>
+    /// Extension methods for <see cref="IDependencyResolver"/>.
+    /// </summary>
+    public static class IDependencyResolverExtensions
+    {
+        /// <summary>
+        /// Resolves dependency of type specified by <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Dependency type.</typeparam>
+        /// <param name="resolver">Dependency resolver.</param>
+        /// <returns>Resolved dependency.</returns>
+        public static T Resolve<T>(this IDependencyResolver resolver)
+        {
+            return (T)resolver.Resolve(typeof(T));
+        }
+    }
+}
