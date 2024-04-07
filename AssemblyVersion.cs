297144b82f3033a1a32575dc89d108926fdcb030
suremaker <wojciech.kotlarski@gmail.com>
2024-04-07 20:27:37 +0100

    Version bump for release

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013-2023")]
-[assembly: AssemblyVersion("3.7.0")]
-[assembly: AssemblyFileVersion("3.7.0")]
-[assembly: AssemblyInformationalVersion("3.7.0")]
+[assembly: AssemblyVersion("3.8.0")]
+[assembly: AssemblyFileVersion("3.8.0")]
+[assembly: AssemblyInformationalVersion("3.8.0")]
