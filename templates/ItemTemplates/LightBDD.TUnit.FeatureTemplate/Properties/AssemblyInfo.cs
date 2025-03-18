4d1e7eb564c6fb9cf973306907de766c90e9a48e
suremaker <wojciech.kotlarski@gmail.com>
2025-03-18 20:45:56 +0000

    #373 Added TUnit templates

---
+++ templates/ItemTemplates/LightBDD.TUnit.FeatureTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LightBDD.TUnit.FeatureTemplate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LightBDD.TUnit.FeatureTemplate")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8b8652d8-a72d-4d29-900d-9783bb753163")]
