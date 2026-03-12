4615631b91f36e035817bc6740d691dcf5a0fc30
NEWDAY\N17781 <aryeh.citron@newday.co.uk>
2026-03-12 17:03:12 +0000

    vsix - item and project templates

---
+++ templates/ItemTemplates/LightBDD.XUnit3.FeatureTemplate/Feature.cs
@@ -0,0 +1,34 @@
+using LightBDD.Framework;
+using LightBDD.Framework.Scenarios;
+using LightBDD.XUnit3;
+
+namespace $rootnamespace$
+{
+	[Label("FEAT-1")]
+	[FeatureDescription(
+@"In order to 
+As a
+I want to ")]
+	public partial class $safeitemname$
+	{
+		[Label("SCENARIO-1")]
+		[Scenario]
+		public void Template_basic_scenario()
+		{
+			Runner.RunScenario(
+				Given_template_method,
+				When_template_method,
+				Then_template_method);
+		}
+
+		[Label("SCENARIO-1")]
+		[Scenario]
+		public void Template_extended_scenario()
+		{
+			Runner.RunScenario(
+				_ => Given_template_method(),
+				_ => When_template_method(),
+				_ => Then_template_method());
+		}
+	}
+}
---
+++ templates/ItemTemplates/LightBDD.XUnit3.FeatureTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LightBDD.XUnit3.FeatureTemplate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LightBDD.XUnit3.FeatureTemplate")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d4f5e6a7-1b2c-3d4e-5f6a-7b8c9d0e1f2a")]
