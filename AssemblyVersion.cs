69fb43249e76cd36c7fef86a3467946e84ab5762
Wojciech <wojciech.kotlarski@wojtechsolutions.com>
2026-06-27 22:42:13 +0200

    Updated project files

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013-2023")]
-[assembly: AssemblyVersion("3.12.0")]
-[assembly: AssemblyFileVersion("3.12.0")]
-[assembly: AssemblyInformationalVersion("3.12.0")]
+[assembly: AssemblyVersion("3.12.1")]
+[assembly: AssemblyFileVersion("3.12.1")]
+[assembly: AssemblyInformationalVersion("3.12.1")]
